702a64b6cc60a6e9d5a10086c8e1337a7fe21ab5
Lachlan Furlong <lachlan@thefurlongs.com>
2019-05-08 17:43:40 +1000

    Added assignment work, after tests

---
+++ ParticleFilter/ParticleList.cpp
@@ -0,0 +1,48 @@
+
+#include "ParticleList.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#define PARTICLE_SIZE      10000
+
+#ifndef DEBUG
+#define DEBUG        0
+#endif
+
+// Initialise a new particle filter with a given maze of size (x,y)
+ParticleList::ParticleList() {
+   numParticles = 0;
+}
+
+// Clean-up the particle list
+ParticleList::~ParticleList() {
+   this->clear();
+}
+
+// Number of particles in the ParticleList
+int ParticleList::getNumberParticles() {
+   return numParticles;
+}
+
+// Get a pointer to the i-th particle in the list
+ParticlePtr ParticleList::get(int i) {
+   return particles[i];
+}
+
+// Add a particle (as a pointer) to the list
+//    This class now has control over the pointer
+//    And should delete the pointer if the particle is removed from the list
+void ParticleList::add_back(ParticlePtr particle) {
+   particles[numParticles] = particle;
+   ++numParticles;
+}
+
+// Remove all particles from the list
+void ParticleList::clear() {
+   for (int i = 0; i < numParticles; i++) {
+      delete particles[i];
+      particles[i] = nullptr;
+   }
+}
